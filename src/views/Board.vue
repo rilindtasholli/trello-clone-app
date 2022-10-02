d2689634aa8c61ffaf76deabbc1ed8b426ffdc6a
rilindtasholli <rilindtasholli@gmail.com>
2022-10-02 17:51:29 +0200

    add and edit tasks

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -15,6 +15,8 @@
             </p>
           </div>
         </div>
+
+        <input type="text" class="block p-2 w-full bg-transparent" placeholder="+ Enter new task" @keyup.enter="createTask($event, column.tasks)">
       </div>
     </div>
 
@@ -39,6 +41,13 @@ export default {
     },
     close () {
       this.$router.push({ name: 'board' })
+    },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', {
+        tasks,
+        name: e.target.value
+      })
+      e.target.value = ''
     }
   }
 }
--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="task-view">
     <div class="flex flex-col flex-grow items-start justify-between px-4">
-      {{ task.name }}
+      <input
+        @change="updateTaskProperty($event, 'name')"
+        @keyup.enter="updateTaskProperty($event, 'name')"
+        type="text" :value="task.name"
+        class="p-2 w-full mr-2 flex-grow text-xl font-bold">
+
+      <textarea @change="updateTaskProperty($event, 'description')" class="relative w-full bg-transparent px-2 border mt-2 h-64 border-none leading-normal" :value="task.description" />
     </div>
   </div>
 </template>
@@ -13,6 +19,15 @@ export default {
     ...mapGetters(['getTask']),
     task () {
       return this.getTask(this.$route.params.id)
+    }
+  },
+  methods: {
+    updateTaskProperty (e, key) {
+      this.$store.commit('UPDATE_TASK', {
+        task: this.task,
+        key,
+        value: e.target.value
+      })
     }
   }
 }

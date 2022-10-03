84a2f35c200618c861e8b1bbf7322645e3999d42
rilindtasholli <rilindtasholli@gmail.com>
2022-10-03 16:38:25 +0200

    code refactor(created AppDrag and AppDrop components to reduce repeated code)

--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -1,25 +1,33 @@
 <template>
-  <div
-    class="task"
-    draggable
-    @dragstart="pickupTask($event, taskIndex, columnIndex)"
-    @click="goToTask(task)"
-    @dragover.prevent
-    @dragenter.prevent
-    @drop.stop="moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)"
-  >
-    <span class="w-full flex-no-shrink font-bold">
-      {{ task.name }}
-    </span>
-    <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
-      {{ task.description }}
-    </p>
-  </div>
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
+      class="task"
+      :transferData="{
+        type: 'task',
+        fromColumnIndex: columnIndex,
+        fromTaskIndex: taskIndex
+      }"
+      @click="goToTask(task)"
+    >
+      <span class="w-full flex-no-shrink font-bold">
+        {{ task.name }}
+      </span>
+      <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+        {{ task.description }}
+      </p>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin.js'
+import AppDrag from './AppDrag.vue'
+import AppDrop from './AppDrop.vue'
 export default {
+  components: {
+    AppDrag,
+    AppDrop
+  },
   props: {
     task: {
       type: Object,
@@ -32,14 +40,6 @@ export default {
   },
   mixins: [movingTasksAndColumnsMixin],
   methods: {
-    pickupTask (e, taskIndex, fromColumnIndex) {
-      e.dataTransfer.effectAllowed = 'move'
-      e.dataTransfer.dropEffect = 'move'
-
-      e.dataTransfer.setData('from-task-index', taskIndex)
-      e.dataTransfer.setData('from-column-index', fromColumnIndex)
-      e.dataTransfer.setData('type', 'task')
-    },
     goToTask (task) {
       this.$router.push({ name: 'task', params: { id: task.id } })
     }
